f33c5cfcc9f2dfd86631171f697a91012ca5de37
codingtmd <fisher.lei@gmail.com>
2015-10-01 23:03:35 -0700

    Update Find_the_Duplicate_Number.cpp

--- src/Find_the_Duplicate_Number.cpp
+++ src/Find_the_Duplicate_Number.cpp
@@ -1,26 +1,26 @@
-ass Solution {
+class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-            int length = nums.size();
-	            
-		            for(int i =0; i< length; i++) {
-			                if(nums[i] == i+1) {
-					                continue;
-							            }
-								                
-										            int oldIndex = i;
-											                int newIndex = nums[i]-1;
-													            while(nums[oldIndex] != oldIndex +1 ) {
-														                    
-																                    if(nums[oldIndex] == nums[newIndex] ) {
-																		                        return nums[oldIndex];
-																					                }
-																							                int temp = nums[newIndex];
-																									                nums[newIndex] = nums[oldIndex];
-																											                nums[oldIndex] = temp;
-																													                
-																															                newIndex = nums[oldIndex] -1;
-																																	            }
-																																		            }
-																																			        }
-																																				};
+    	int findDuplicate(vector<int>& nums) {
+	        int length = nums.size();
+		            
+		for(int i =0; i< length; i++) {
+			if(nums[i] == i+1) {
+				continue;
+			}
+									                
+			int oldIndex = i;
+			int newIndex = nums[i]-1;
+			while(nums[oldIndex] != oldIndex +1 ) {
+		                if(nums[oldIndex] == nums[newIndex] ) {
+				        return nums[oldIndex];
+				}
+				int temp = nums[newIndex];
+				nums[newIndex] = nums[oldIndex];
+				nums[oldIndex] = temp;
+				
+				newIndex = nums[oldIndex] -1;
+			}
+		}
+    	}
+	
+};
