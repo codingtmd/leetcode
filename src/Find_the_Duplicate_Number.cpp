d0eea173a59c4520cb102c685ba13f827c0c781b
codingtmd <leizhang_iscas@hotmail.com>
2015-10-01 22:58:38 -0700

    add code

---
+++ src/Find_the_Duplicate_Number.cpp
@@ -0,0 +1,26 @@
+ass Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+            int length = nums.size();
+	            
+		            for(int i =0; i< length; i++) {
+			                if(nums[i] == i+1) {
+					                continue;
+							            }
+								                
+										            int oldIndex = i;
+											                int newIndex = nums[i]-1;
+													            while(nums[oldIndex] != oldIndex +1 ) {
+														                    
+																                    if(nums[oldIndex] == nums[newIndex] ) {
+																		                        return nums[oldIndex];
+																					                }
+																							                int temp = nums[newIndex];
+																									                nums[newIndex] = nums[oldIndex];
+																											                nums[oldIndex] = temp;
+																													                
+																															                newIndex = nums[oldIndex] -1;
+																																	            }
+																																		            }
+																																			        }
+																																				};
