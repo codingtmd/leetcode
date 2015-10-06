38d40a8b8654b0f8962a88166f946edb45793f57
codingtmd <leizhang_iscas@hotmail.com>
2015-10-05 21:45:01 -0700

    fix for solution #263

--- src/Ugly_Number.cpp
+++ src/Ugly_Number.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     bool isUgly(int num) {
-        if(num <= 0) return false;
-        
-        int arr [] = { 2, 3, 5};
-	    vector<int> factors(arr, arr + sizeof(arr) / sizeof(arr[0]));
-        return ugly(num,factors);
-    }
-    
-    bool ugly(int num, vector<int>& factors) {
-        if(num == 1) {
-            return true;
-        }
-        
-        for(int i =0; i< factors.size(); i++) {
-            if(num % factors[i] != 0) {
-                continue;
-            }
-            if(ugly(num/factors[i], factors)) {
-                return true;
-            } 
-        }
-        return false;
+        if (num <= 0) return false;
+        while (num % 2 == 0) num /= 2;
+        while (num % 3 == 0) num /= 3;
+        while (num % 5 == 0) num /= 5;
+        return num == 1;
     }
 };
